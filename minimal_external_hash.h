64f57e6e574ebcc8d0efb978e1f1b7e82cd97b52
Simon Berger <simberger@gmail.com>
2013-01-27 19:09:02 +0100

    moved disk_hash code ot 'minimal external hash' library.

--- minimal_external_hash.h
+++ minimal_external_hash.h
@@ -27,9 +27,8 @@
 #include <vector>
 #include <iterator>
 #include <stdexcept>
-#include "minimal_external_hash.h"
 
-#if 0
+namespace meh {
 class mapped_file {
 public:
     mapped_file( const char *filename, bool read_write ) {
@@ -72,7 +71,8 @@ private:
     int fd_;
     off_t size_;
 };
-
+    
+    
 template<typename K>
 uint64_t hash_value( const K &k ) {
     return k.hash();
@@ -101,16 +101,23 @@ uint64_t hash_value( const char *s ) {
 }
 
 
-class hash_builder {
+template<typename T>
+static T align( T v ) {
+    const size_t a = 32; // FIXME: magic number
+    
+    return v + (a - (size_t(v) % a)) % a;
+}
+
+class file_based_builder {
     typedef int64_t int_type;
     const static size_t int_size = sizeof(int_type);
 public:
-    hash_builder( size_t table_size ) : table_size_(table_size), link_offsets_(table_size), num_extra_hops_(0) {
+    file_based_builder( size_t table_size ) : table_size_(table_size), next_pointers_(table_size), num_extra_hops_(0) {
         
-        // link_offsets_ stores the 'next pointers' per bucket. Initially they point to
+        // next_pointers_ stores the 'next pointers' per bucket. Initially they point to
         // entries in the hash table.
         for( size_t i = 0; i < table_size_; ++i ) {
-            link_offsets_[i] = i * int_size;
+            next_pointers_[i] = i * int_size;
         }
         
         // start appending after the hash table
@@ -121,10 +128,10 @@ public:
         {
             std::ofstream os( filename );
             os.seekp( append_pos_ );
-	    
-	    int_type x = table_size_;
-	    
-	    // write the table size at the end of the file
+            
+            int_type x = table_size_;
+            
+            // write the table size at the end of the file
             // this will also grow the file to the necessary size
             os.write( (char*)(&x), sizeof(int_type));
         }
@@ -138,11 +145,8 @@ public:
             // write name (0 terminated)
             const std::string &name = it->first;
             char *append_pos = base + it->second;
-            
-//             std::cout << "append_pos " << it->second << "\n";
-            
             std::copy( name.begin(), name.end(), append_pos );
-            
+           
             append_pos += name.size();
             *append_pos = 0; ++append_pos;
             
@@ -165,16 +169,15 @@ public:
             is.read( append_pos, size );
         }
         
-        // write bucket chains (i.e., the 'next pointers')
-        for( std::vector< std::pair< size_t, size_t > >::iterator it = ptrs_.begin(); it != ptrs_.end(); ++it ) {
+        // write hash chains (i.e., the 'next pointers')
+        for( std::vector< std::pair< size_t, size_t > >::iterator it = chain_links_.begin(); it != chain_links_.end(); ++it ) {
             int_type x = it->second;
             char *xcp = (char*)&x;
             
             std::copy( xcp, xcp + int_size, base + it->first );
         }
         
-        std::cout << "fill factor: " << ptrs_.size() / float(table_size_) << "\n";
-        std::cout << "avg hops: " << (ptrs_.size() + num_extra_hops_) / float( ptrs_.size()) << "\n";
+        
         
     }
     
@@ -190,16 +193,16 @@ public:
         size_t bucket = name_hash % table_size_;
         
         // remember current 'next pointer' for writing later 
-        ptrs_.push_back( std::make_pair( link_offsets_[bucket], append_pos_ ) );
+        chain_links_.push_back( std::make_pair( next_pointers_[bucket], append_pos_ ) );
         
         
         // statistics: count as 'extra hop' if link_offset_[bucket] does not point into hash table (i.e., the bucket is non-empty)
-        if( link_offsets_[bucket] >= table_size_ * int_size ) {
+        if( next_pointers_[bucket] >= table_size_ * int_size ) {
             ++num_extra_hops_;
         }
         
         // update 'next pointer'
-        link_offsets_[bucket] = append_pos_;
+        next_pointers_[bucket] = append_pos_;
         
         // remember file content location (starts after the chaining link)
         size_t file_pos = append_pos_ + int_size;
@@ -213,12 +216,11 @@ public:
         return true;
     }
     
-    template<typename T>
-    static T align( T v ) {
-        const size_t a = 32;
-        
-        return v + (a - (size_t(v) % a)) % a;
+    void print_stats() {
+        std::cout << "fill factor: " << chain_links_.size() / float(table_size_) << "\n";
+        std::cout << "avg hops: " << (chain_links_.size() + num_extra_hops_) / float( chain_links_.size()) << "\n";   
     }
+    
 private:
     
     std::streampos file_size( const char *filename ) {
@@ -232,19 +234,20 @@ private:
     }
     
     const size_t table_size_;
-    std::vector<size_t> link_offsets_;
+    std::vector<size_t> next_pointers_;
     std::vector<std::pair<std::string,size_t>> file_dest_;
-    std::vector<std::pair<size_t,size_t>> ptrs_;
+    std::vector<std::pair<size_t,size_t>> chain_links_;
     size_t append_pos_;
     size_t num_extra_hops_;
 };
 
-class disk_hash {
+
+class hash {
 public:
     typedef int64_t int_type;
     const static size_t int_size = sizeof(int_type);
     
-    disk_hash( const char *filename ) : mf_(filename, false) {
+    hash( const char *filename ) : mf_(filename, false) {
         base_ = (char *)mf_.base();
         size_t s = mf_.size();
         
@@ -253,7 +256,7 @@ public:
         
         std::cout << "table size: " << ts << "\n";
         table_size_ = ts;
-        assert( base_ == hash_builder::align(base_));
+        assert( base_ == align(base_));
     }
     
     char *find( const char *filename, size_t &out_size ) {
@@ -277,14 +280,13 @@ public:
                 
                 size_t size = get_int( base_ + offset );
                 offset += int_size;
-                offset = hash_builder::align(offset);
+                offset = align(offset);
 //                 std::cout << "found: " << filename << " chunk size: " << size << " at " << std::hex << offset << std::dec << " chain: " << chain_len << "\n";
                 if( remaining_size( offset ) < size ) {
                     throw std::runtime_error( "internal error. premature end of hash file." );
                 }
                 out_size = size;
                 return base_ + offset;
-                
             }
             ++chain_len;
             offset = next_offset;
@@ -322,66 +324,6 @@ private:
     mapped_file mf_;
     char *base_;
 };
-#endif
 
-int main() {
-        
-    //const size_t ht_size = 1024 * 32;
-    
-    const size_t ht_size = 1024 * 64;
-    if( false )
-    {
-        meh::file_based_builder hb( ht_size );
-    
-        {
-            std::ifstream is( "files.txt" );
-            
-            while( is.good() ) {
-                std::string name;
-                is >> name;
-                
-//                 std::cout << "name: " << name << "\n";
-                
-                if( !name.empty() ) {
-                    
-                    std::ifstream ts( name );
-                    
-                    if( ts.good() ) {
-                        hb.add( name.c_str() );
-                    }
-                }
-            }
-            
-        }
-        hb.write( "hash.bin" );
-    }
-    
-    size_t nfound = 0;
-    {
-        meh::hash dh( "hash.bin" );
-        std::ifstream is( "files.txt" );
-        
-        while( is.good() ) {
-            std::string name;
-            is >> name;
-            
-//             std::cout << "name: " << name << "\n";
-            
-            if( !name.empty() ) {
-                size_t size;
-                char *data = dh.find( name.c_str(), size );
-                if( data != 0 ) {
-                    nfound++;
-                }
-                
-            }
-        }
-        
-    }
-    
-    std::cout << "found: " << nfound << "\n";
-     
-    
-//     mapped_file mf( "test.bin", false );
-    
+
 }
